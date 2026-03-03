f62f2775ef334805cea35efd4de54b090a5639fd
Yike_Lin <3100281535@qq.com>
2026-03-03 20:19:18 +0800

    解析单块电池的.mat数据

--- dataloader/xjtu_loader.py
+++ dataloader/xjtu_loader.py
@@ -35,4 +35,48 @@ class XJTUDataset:
         else:
             data_norm = scaler.minmax(feature_range=self.minmax_range)
         return data_norm
+    
+    """
+    解析单块电池的.mat数据
+    """
+    def _parser_mat_data(self , battery_i_mat):
+        # 1. 初始化列表
+        data_list = []          # 装时间序列特征    
+        cap_list = []           # 装每一圈cycle测出来的实际容量
+
+        # 2. 核心循环：一圈圈拆数据
+        for i in range(battery_i_mat.shape[1]): # battery_i_mat.shape[1] 是该电池总循环次数
+            cycle_i = battery_i_mat[0 , i]      # cycle_i 是当前第 i 圈的所有数据包
+
+            time = cycle_i['relative_time_min']
+            current = cycle_i['current_A']
+            voltage = cycle_i['voltage_V']
+            temp = cycle_i['temperature_C']
+
+            capacity = cycle_i['capacity'][0]
+            cap_list.append(capacity)
+
+            cycle_arr = np.concatenate([time , current , voltage , temp] , axis=0)
+            data_list.append(cycle_arr)
+
+        # 3. 所有圈数合并：(4 , L) -> (N , 4 , L)
+        data = np.asarray(data_list , dtype=np.float32)
+        label = np.asarray(cap_list , dtype=np.float32)
+
+        # 4. 归一化，贴标签
+        data = self._normalize(data)
+        soh = label / self.max_capacity
+
+        return data,soh
+
+
+        
+
+
+
+
+
+
+
+
 
