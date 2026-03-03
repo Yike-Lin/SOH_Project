0188c89475e616408d1dab5ae2ba88ebe73c577e
Yike_Lin <3100281535@qq.com>
2026-03-03 19:47:55 +0800

    初始化

---
+++ dataloader/xjtu_loader.py
@@ -0,0 +1,26 @@
+from scipy.io import loadmat
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
+import os
+
+from utils.scaler import Scaler
+
+
+
+class XJTUDataset:
+    """
+    初始化,保存一些配置参数
+    """
+    def __init__(self , args):
+        self.root = 'data/XUTU'
+        self.max_capacity = 2.0
+
+        self.normalized_type = args.normalized_type
+        self.minmax_range = args.minmax_range
+        self.seed = args.random_seed
+        self.batch = args. batch                        # 实验批次
+        self.batch_size = args.batch_size               # 训练批次
+
