f1ffd774805cf62b51ff4fc2e0f198f14fc71f29
Yike_Lin <3100281535@qq.com>
2026-03-04 17:35:02 +0800

    数据和加载数据的接口

--- dataloader/xjtu_loader.py
+++ dataloader/xjtu_loader.py
@@ -15,7 +15,7 @@ class XJTUDataset:
     初始化,保存一些配置参数
     """
     def __init__(self , args):
-        self.root = 'data/XUTU'
+        self.root = 'data/XJTU'
         self.max_capacity = 2.0
 
         self.normalized_type = args.normalized_type
@@ -145,10 +145,91 @@ class XJTUDataset:
         # 4. 封装 返回
         return self._encapsulation(train_x , train_y , test_x , test_y)
 
+    
+    """
+    加载完整充电曲线数据接口
+    """
+    def get_charge_data(self , test_battery_id = 1):
+        file_name = f'batch-{self.batch}.mat'
+        path = os.path.join(self.root , 'charge' , file_name)
+
+        train_loader , valid_laoder , test_loader = self._get_raw_data(path , test_battery_id)
+        return {'train': train_loader , 'valid': valid_laoder , 'test': test_loader}
+    
+    """
+    加载部分充电曲线数据接口
+    """
+    def get_partial_data(self , test_battery_id = 1):
+        if self.batch == 6:
+            file_name = f'batch-{self.batch}_3.9-4.19.mat'
+        else:
+            file_name = f'batch-{self.batch}_3.7-4.1.mat'
+
+        path = os.path.join(self.root, 'partial_charge', file_name)
+        train_loader, valid_loader, test_loader = self._get_raw_data(path, test_battery_id)
+        return {'train': train_loader, 'valid': valid_loader, 'test': test_loader}
+
+    """
+    解析电池在 Excel 表中的特征数据
+    """
+    def _parser_xlsx(self , df_i: pd.DataFrame):
+        x = np.asarray(df_i.iloc[:, :-1], dtype=np.float32)
+        label = np.asarray(df_i['label'], dtype=np.float32)
+        x = self._normalize(x)
+        soh = label / self.max_capacity
+        return x, soh
+    
+    """
+    加载手工特征数据
+    """
+    def get_features(self , test_battery_id = 1):
+        file_name = f'batch-{self.batch}_features.xlsx'
+        path = os.path.join(self.root, 'handcraft_features', file_name)
+
+        df_dict = pd.read_excel(path, sheet_name=None)
+        sheet_names = list(df_dict.keys())
+        battery_ids = list(range(1, len(sheet_names) + 1))
+
+        if test_battery_id not in battery_ids:
+            raise IndexError(f'test_battery_id must be in {battery_ids}, got {test_battery_id}')
+
+        test_df = pd.read_excel(path, sheet_name=test_battery_id - 1, header=0)
+        test_x, test_y = self._parser_xlsx(test_df)
+
+        train_x_list, train_y_list = [], []
+        for bid in battery_ids:
+            if bid == test_battery_id:
+                continue
+            df_i = df_dict[sheet_names[bid - 1]]
+            x, y = self._parser_xlsx(df_i)
+            train_x_list.append(x)
+            train_y_list.append(y)
+
+        train_x = np.concatenate(train_x_list, axis=0)
+        train_y = np.concatenate(train_y_list, axis=0)
+
+        return self._encapsulation(train_x, train_y, test_x, test_y)
+    
 
 
+if __name__ == '__main__':
+    import argparse
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--random_seed', type=int, default=2023)
+    parser.add_argument('--normalized_type', type=str, default='minmax')
+    parser.add_argument('--minmax_range', type=tuple, default=(-1, 1))
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--batch', type=int, default=1)
+    args = parser.parse_args([])  # [] 表示不用命令行参数，使用默认值
 
+    dataset = XJTUDataset(args)
+    loaders = dataset.get_charge_data(test_battery_id=1)
+
+    for split in ['train', 'valid', 'test']:
+        dl = loaders[split]
+        x, y = next(iter(dl))
+        print(split, 'batch x shape:', x.shape, 'batch y shape:', y.shape)
         
 
 
